d1ccc9deff5259aa9be3ae4a2edb5824604b5811
Nichakan <nichakan4740@gmail.com>
2022-05-05 23:46:30 +0700

    add-pack

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import VueSweetalert2 from 'vue-sweetalert2'
 import 'sweetalert2/dist/sweetalert2.min.css'
+import 'moment'
+import 'moment-timezone'
 
 const app = createApp(App)
 app.use(router)
