4c4e1687534dfe6ae6b0dbc5b890e74e754a523d
Nichakan <nichakan4740@gmail.com>
2022-05-08 05:22:48 +0700

    add

--- src/views/showeventall.vue
+++ src/views/showeventall.vue
@@ -7,7 +7,7 @@ import moment from "moment";
 const events = ref([]);
 
 onBeforeMount(async () => {
-  const res = await fetch(`${import.meta.env.VITE_BASE_URL}/api/booking`);
+  const res = await fetch('http://10.4.56.91:8080//api/booking');
   if (res.status === 200) {
     events.value = await res.json();
     console.log(events.value);
