a59991923b59a959b42753fd2d9e778abcb05b71
Nichakan <nichakan4740@gmail.com>
2022-05-08 13:53:29 +0700

    add

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import showeventall from '../views/showeventall.vue'
 import showeventwithid from '../views/showeventwithid.vue'
 
 
-const history = createWebHistory()
+const history = createWebHistory(`${import.meta.env.VITE_BASE_URL}`)
 
 const routes = [
   
