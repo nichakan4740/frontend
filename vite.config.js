0d738622eb0f8a1ea270decf2452a9f9600ace12
Nichakan <nichakan4740@gmail.com>
2022-05-08 13:01:24 +0700

    add

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
-  base: '/kw2/',
-  server: {
-    proxy: {
-    '/api': {
-    target: 'http://intproj21.sit.kmutt.ac.th',
-    changeOrigin: true,
-    secure: false,
-    rewrite: (path) => path.replace(/^\/api/, '')
-     }
-    }
-  }
+  base: '/kw2/'
 })
 
 
