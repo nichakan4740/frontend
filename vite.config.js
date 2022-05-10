01c6d05e601c956a75b22b1389c1c44f35b69cf0
Nichakan <nichakan4740@gmail.com>
2022-05-10 13:31:15 +0700

    add

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
-  base:'/kw2/',
+  base:'/kw2/'/* ,
   server:{
    proxy:{
      '/api':{
@@ -12,7 +12,7 @@ export default defineConfig({
       rewrite: (path) => path.replace(/^\/api/, '')
      }
    }
-  }
+  } */
 })
 
 
