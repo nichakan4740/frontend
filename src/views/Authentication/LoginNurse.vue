de6176e7662d4156bede3d8613ec6e0b916565f5
nichakan <nichakan4740@gmail.com>
2024-01-22 00:19:40 +0700

    add_commit

--- src/views/Authentication/LoginNurse.vue
+++ src/views/Authentication/LoginNurse.vue
@@ -5,25 +5,26 @@ import { useRouter } from "vue-router";
 <template>
 <div class="container mx-auto">
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <p class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+      <p class="flex items-center mb-6 text-2xl font-semibold text-gray-900 ">
           Tele-Nursing System  
       </p>
-       <p class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+       <p class="flex items-center mb-6 text-2xl font-semibold text-gray-900 ">
           ระบบพยาบาลทางไกล
       </p>
-      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+
+      <div class="w-full  rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 bg-white ">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  พยาบาล
+              <h1 class="text-xl font-bold text-center leading-tight tracking-tight text-gray-900 md:text-2xl ">
+                  การเข้าสู่ระบบของพยาบาลพยาบาล
               </h1>
               <form class="space-y-4 md:space-y-6" action="#">
                   <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">เลขใบประกอบวิชาชีพพยาบาล</label>
-                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="xxxxxxxxxxx" required="">
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">เลขใบประกอบวิชาชีพพยาบาล</label>
+                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="xxxxxxxxxxx" required="">
                   </div>
                   <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
+                      <input type="password" name="password" id="password" placeholder="xxxxxxxx" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                   </div>
                   <div class="flex items-center justify-between">
                       <div class="flex items-start">
@@ -36,19 +37,18 @@ import { useRouter } from "vue-router";
                       </div>
                       <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                   </div>
-                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Don't have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
-                  </p>
 
-     <router-link :to="{ name: 'registerNurse' }">
-      <button
-        type="button"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-      >
-      ลงทะเบียน
-      </button>
+                <div>
+                     <button type="submit" class=" block w-full bg-blue-900 hover:bg-blue-800 text-white font-bold py-2 px-4 mb-4 border rounded">เข้าสู่ระบบ</button>
+                </div>
+                
+    <div class="text-center">
+      <router-link :to="{ name: 'registerNurse' }">
+      <p class="text-sm font-medium  text-gray-500 ">คุณไม่ม่บัญชีใช่หรือไม่?
+      <a class=" font-bold text-primary-600 hover:underline text-indigo-800">โปรดลงทะเบียน</a>
+      </p>
     </router-link>
+    </div>
               </form>
           </div>
       </div>
@@ -64,4 +64,7 @@ import { useRouter } from "vue-router";
 </template>
 
 
-<style></style>
+<style>
+
+
+</style>
