7788f6a16f03c9d56382f069a48f8dc0490766e6
Tripuwarej <tripuwarej.paw@gmail.com>
2024-03-02 21:34:00 +0700

    add pusher in header

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -10,6 +10,32 @@ const navigation = [
   // { name: "ฟีเจอร์แนะนำ", href: "/Features" }
 ];
 const mobileMenuOpen = ref(false);
+
+
+// เพิ่ม pusher
+const pusherScript = document.createElement('script');
+pusherScript.src = 'https://js.pusher.com/8.2.0/pusher.min.js';
+document.head.appendChild(pusherScript);
+
+// Pusher initialization code
+pusherScript.onload = () => {
+  window.Pusher.logToConsole = true;
+  var pusher = new window.Pusher('d0537e1c7c0b105eaa20', {
+    cluster: 'ap1'
+  });
+  var channel = pusher.subscribe('user-milk');
+  channel.bind('my-event', function(data) {
+    // Assuming `app` is defined somewhere else
+    alert('my-event')
+  });
+  channel.bind('live-chat', function(data) {
+    // Assuming `app` is defined somewhere else
+    alert('live-chat'+data)
+    console.log(data)
+  });
+};
+ 
+
 </script>
 
 <template>
