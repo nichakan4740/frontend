f9fdade546c8b00d6c7e51b31df69c8e40762c0f
nichakan <nichakan4740@gmail.com>
2024-05-09 04:25:00 +0700

    add_commit

--- src/views/GroupChatNurse.vue
+++ src/views/GroupChatNurse.vue
@@ -279,13 +279,15 @@ const firstCharacter = computed(() => {
                 </div>
 <!-- ---------------------------------------------------------------------------------------------------- -->
               
-                <div class="col-start-6 col-end-13 p-3 rounded-lg">
+                <div class="col-start-6 col-end-13 p-3 rounded-lg" v-for="(message, index) in messagesendToUser" :key="index" >
                   <div class="flex items-center justify-start flex-row-reverse">
                     <div class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
                           {{ firstCharacter }} 
                     </div>
                     <div class="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
-                      <div>I'm ok what about you?</div>
+                      <div>{{ message.message }}</div>
+                      <p class="text-sm text-gray-600">CreatedAt:{{ message.createdAt }}</p>
+                      <p class="text-sm text-gray-600">admin_id: {{ message.user_id}}</p>
                     </div>
                   </div>
                 </div>
@@ -293,7 +295,7 @@ const firstCharacter = computed(() => {
 
 <div>
     <!-- ส่วนที่แสดงผลข้อความที่ตอบกลับมาจาก user -->
-    <div class="mt-4">
+   <!--  <div class="mt-4">
       <h2 class="text-lg font-semibold">Messages from User</h2>
       <div class="mt-2">
         <div v-for="msg in messagefromUser" :key="msg.createdAt">
@@ -303,18 +305,7 @@ const firstCharacter = computed(() => {
           </div>
         </div>
       </div>
-    </div>
-    
-    <!-- ส่วนที่แสดงผลข้อความที่ตอบกลับไปยัง user -->
-    <div class="mt-4">
-      <h2 class="text-lg font-semibold">Messages to User</h2>
-      <div class="mt-2">
-       <div v-for="(message, index) in messagesendToUser" :key="index" class="bg-gray-100 p-2 rounded-md mb-2">
-          <p>{{ message.message }}</p>
-          <p class="text-xs text-gray-500">{{ message.createdAt }}</p>
-        </div>
-      </div>
-    </div>
+    </div> -->
 </div>
 <!-- --------------------------------------------------------------------------------------------------------- -->
            
